01e4f55d658e4868df098d18da07eed32df3c3c0
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-07-07 20:38:06 +0800

    routes added
    added routes for admin to see users and edit them, also for routes on user roles be set and viewed

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,12 @@ Route::middleware('auth')->group(function(){
 
     Route::get('/admin', [AdminController::class, 'index'])->name('dash')->middleware('role:admin');
     Route::get('/admin/users',[AdminController::class,'manageUsers'])->name('usertool')->middleware('role:admin');
+    Route::patch('/admin/users/{id}', [AdminController::class, 'updateUser'])->name('updateusr')->middleware('role:admin');
+    Route::delete('/admin/users/{id}', [AdminController::class, 'rmUser'])->name('rmusr')->middleware('role:admin');
+
+    Route::post('/admin/user_roles/{id}',[AdminController::class,'addUsrRole'])->name('usr_roles.create')->middleware('role:admin');
+    Route::get('/admin/user_roles/{id}',[AdminController::class,'manageUsrRoles'])->name('usr_roles.view')->middleware('role:admin');
+    Route::delete('/admin/{user_id}/user_roles/{role_id}', [AdminController::class, 'rmUsrRole'])->name('usr_roles.rmrole')->middleware('role:admin');
 
     Route::get('/acctg/new',[BookController::class,'newLedgerEntry'])->middleware(['role:admin,bookeeper,auditor'])->name('newledger');
     Route::post('/acctg/new',[BookController::class,'saveNewLedgerEntry'])->middleware('role:admin,bookeeper')->name('saveledger');
